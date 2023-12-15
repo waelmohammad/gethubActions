de11ff281049d0cbd8b028ecde4c345607192b8d
wael al mohammad <wail84n@gmail.com>
2023-12-15 13:29:13 +0300

    push all changes

---
+++ GitHubActions/GitHubActionsUITests/GitHubActionsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitHubActionsUITestsLaunchTests.swift
+//  GitHubActionsUITests
+//
+//  Created by wael on 12/15/23.
+//
+
+import XCTest
+
+class GitHubActionsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
