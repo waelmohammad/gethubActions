cfb7accc4b7001e3d7334e291bec2ee3fb750a0a
wael al mohammad <wail84n@gmail.com>
2023-12-15 21:25:47 +0300

    sdsds

--- GitHubActions/ViewController.swift
+++ GitHubActions/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("waeccl")
+        print("sdsds")
         // Do any additional setup after loading the view.
     }
 
