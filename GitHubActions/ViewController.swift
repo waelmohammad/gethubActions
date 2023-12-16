b84b5688891a08b6b3bd25c4e581e1668b62568c
wael al mohammad <wail84n@gmail.com>
2023-12-16 22:24:18 +0300

    upload new build

--- GitHubActions/ViewController.swift
+++ GitHubActions/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("sdsdsd")
+        print("xcxcxcx")
         // Do any additional setup after loading the view.
     }
 
